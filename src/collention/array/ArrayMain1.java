b4bf1be980b74de4e4ee3e0ad46f796e0131bf11
onepunchkim <hyukjin4711@gmail.com>
2024-10-10 18:21:41 +0900

    array study

--- src/collention/array/ArrayMain1.java
+++ src/collention/array/ArrayMain1.java
@@ -5,24 +5,24 @@ import java.util.Arrays;
 public class ArrayMain1 {
     public static void main(String[] args) {
         int[] arr = new int[5];
-        //index 입력: 0(1)
-        System.out.println("== index 입력: 0(1) ==");
+        //index 입력: O(1)
+        System.out.println("== index 입력: O(1) ==");
         arr[0] = 1;
         arr[1] = 2;
         arr[2] = 3;
         System.out.println(Arrays.toString(arr));
 
-        //index 변경: 0(1)
-        System.out.println("== index 변경: 0(1) ==");
+        //index 변경: O(1)
+        System.out.println("== index 변경: O(1) ==");
         arr[2] = 10;
         System.out.println(Arrays.toString(arr));
 
-        //index 조회: 0(1)
-        System.out.println("== index 조회: 0(1) ==");
+        //index 조회: O(1)
+        System.out.println("== index 조회: O(1) ==");
         System.out.println("arr[2] = " + arr[2]);
 
-        //검색: 0(n)
-        System.out.println("== 배열 검색: 0(n) ==");
+        //검색: O(n)
+        System.out.println("== 배열 검색: O(n) ==");
         System.out.println(Arrays.toString(arr));
         int value = 10;
         for (int i = 0; i < arr.length; i++) {
