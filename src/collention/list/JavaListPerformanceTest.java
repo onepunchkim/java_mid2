1250e8afe1489c67dff68af045c7dabfdc22b54f
onepunchkim <hyukjin4711@gmail.com>
2024-10-15 16:33:54 +0900

    list study

--- src/collention/list/JavaListPerformanceTest.java
+++ src/collention/list/JavaListPerformanceTest.java
@@ -1,44 +1,48 @@
 package collention.list;
 
-public class MyListPerformanceTest {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class JavaListPerformanceTest {
     public static void main(String[] args) {
         int size = 50_000;
-        System.out.println("==MyArrayList 추가==");
-        addFirst(new MyArrayList<>(), size);
-        addMid(new MyArrayList<>(), size); //찾는데 O(1), 데이터 추가(밀기) O(n)
-        MyArrayList<Integer> arrayList = new MyArrayList<>(); //조회용 데이터로 추가
+        System.out.println("==ArrayList 추가==");
+        addFirst(new ArrayList<>(), size);
+        addMid(new ArrayList<>(), size); //찾는데 O(1), 데이터 추가(밀기) O(n)
+        ArrayList<Integer> arrayList = new ArrayList<>(); //조회용 데이터로 추가
         addLast(arrayList, size); //찾는데 O(1), 데이터 추가(밀기) O(1)
 
-        System.out.println("==MyLinkedList 추가==");
-        addFirst(new MyLinkedList<>(), size);
-        addMid(new MyLinkedList<>(), size); //찾는데 O(n), 데이터 추가 O(1)
-        MyLinkedList<Integer> linkedList = new MyLinkedList<>(); //조회용 데이터로 추가
-        addLast(linkedList, size); //찾는데 O(n), 데이터 추가 O(1)
-
         int loop = 10000;
-        System.out.println("==MyArrayList 조회==");
+        System.out.println("==ArrayList 조회==");
         getIndex(arrayList, loop, 0);
         getIndex(arrayList, loop, size / 2);
         getIndex(arrayList, loop, size - 1);
 
-        System.out.println("==MyLinkedList 조회==");
-        getIndex(linkedList, loop, 0);
-        getIndex(linkedList, loop, size / 2);
-        getIndex(linkedList, loop, size - 1);
-
-        System.out.println("==MyArrayList 검색==");
+        System.out.println("==ArrayList 검색==");
         search(arrayList, loop, 0);
         search(arrayList, loop, size / 2);
         search(arrayList, loop, size - 1);
 
-        System.out.println("==MyLinkedList 검색==");
+        System.out.println("==LinkedList 추가==");
+        addFirst(new LinkedList<>(), size);
+        addMid(new LinkedList<>(), size); //찾는데 O(n), 데이터 추가 O(1)
+        LinkedList<Integer> linkedList = new LinkedList<>(); //조회용 데이터로 추가
+        addLast(linkedList, size); //찾는데 O(1), 데이터 추가 O(1)
+
+        System.out.println("==LinkedList 조회==");
+        getIndex(linkedList, loop, 0);
+        getIndex(linkedList, loop, size / 2);
+        getIndex(linkedList, loop, size - 1);
+
+        System.out.println("==LinkedList 검색==");
         search(linkedList, loop, 0);
         search(linkedList, loop, size / 2);
         search(linkedList, loop, size - 1);
 
     }
 
-    private static void addFirst(MyList<Integer> list, int size) {
+    private static void addFirst(List<Integer> list, int size) {
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < size; i++) {
             list.add(0, i);
@@ -47,7 +51,7 @@ public class MyListPerformanceTest {
         System.out.println("앞에 추가 - 크기: " + size + ", 계산시간: " + (endTime - startTime) + "ms");
     }
 
-    private static void addMid(MyList<Integer> list, int size) {
+    private static void addMid(List<Integer> list, int size) {
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < size; i++) {
             list.add(i / 2, i);
@@ -56,7 +60,7 @@ public class MyListPerformanceTest {
         System.out.println("평균 추가 - 크기: " + size + ", 계산시간: " + (endTime - startTime) + "ms");
     }
 
-    private static void addLast(MyList<Integer> list, int size) {
+    private static void addLast(List<Integer> list, int size) {
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < size; i++) {
             list.add(i);
@@ -65,7 +69,7 @@ public class MyListPerformanceTest {
         System.out.println("뒤에 추가 - 크기: " + size + ", 계산시간: " + (endTime - startTime) + "ms");
     }
 
-    private static void getIndex(MyList<Integer> list, int loop, int index) {
+    private static void getIndex(List<Integer> list, int loop, int index) {
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < loop; i++) {
             list.get(index);
@@ -74,7 +78,7 @@ public class MyListPerformanceTest {
         System.out.println("index: " + index + ", 반복: " + loop + ", 계산시간: " + (endTime - startTime) + "ms");
     }
 
-    private static void search(MyList<Integer> list, int loop, int findValue) {
+    private static void search(List<Integer> list, int loop, int findValue) {
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < loop; i++) {
             list.indexOf(findValue);
