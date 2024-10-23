d9d0153d83ccd5600f7185eaef009bcd51965fa7
onepunchkim <hyukjin4711@gmail.com>
2024-10-23 17:39:22 +0900

    hash study

---
+++ src/collention/set/member/MemberNoHashNoEq.java
@@ -0,0 +1,20 @@
+package collention.set.member;
+
+public class MemberNoHashNoEq {
+    private String id;
+
+    public MemberNoHashNoEq(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberNoHashNoEq{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+}
