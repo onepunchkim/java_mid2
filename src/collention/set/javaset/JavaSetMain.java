d97216cc44e3e91088010c3fe206065979d477dd
onepunchkim <hyukjin4711@gmail.com>
2024-10-24 17:24:32 +0900

    set study

---
+++ src/collention/set/javaset/JavaSetMain.java
@@ -0,0 +1,26 @@
+package collention.set.javaset;
+
+import java.util.*;
+
+public class JavaSetMain {
+    public static void main(String[] args) {
+        run(new HashSet<>()); //O(1)
+        run(new LinkedHashSet<>()); //O(1)
+        run(new TreeSet<>()); //O(log N)
+    }
+
+    private static void run(Set<String> set) {
+        System.out.println("set = " + set.getClass());
+        set.add("C");
+        set.add("B");
+        set.add("A");
+        set.add("1");
+        set.add("2");
+
+        Iterator<String> iterator = set.iterator();
+        while(iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+    }
+}
