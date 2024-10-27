37524762f0e95512a73ed28963b4227ff7d499e5
onepunchkim <hyukjin4711@gmail.com>
2024-10-27 16:37:39 +0900

    map study

---
+++ src/collention/map/MapMain2.java
@@ -0,0 +1,24 @@
+package collention.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMain2 {
+    public static void main(String[] args) {
+        Map<String, Integer> studentMap = new HashMap<>();
+
+        //학생 성적 데이터 추가
+        studentMap.put("studentA", 90);
+        System.out.println(studentMap);
+
+        studentMap.put("studentA", 100); //같은 키에 저장 시 기존 값 교체
+        System.out.println(studentMap);
+
+        boolean containskey = studentMap.containsKey("studentA");
+        System.out.println("containskey = " + containskey);
+
+        //특정 학생의 값 삭제
+        studentMap.remove("studentA");
+        System.out.println(studentMap);
+    }
+}
