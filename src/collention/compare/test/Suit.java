bdc52d099d4019642e1089c7b0962ab758a7249d
성스러운 성진이 <hyukjin4711@gmail.com>
2024-10-31 21:14:03 +0900

    compare study

--- src/collention/compare/test/Suit.java
+++ src/collention/compare/test/Suit.java
@@ -1,10 +1,10 @@
 package collention.compare.test;
 
 public enum Suit {
-    SPADE("\u2660"), //스페이드
-    HEART("\u2665"), //하트
-    DIAMOND("\u2666"), //다이아몬드
-    CLUB("\u2663"); //클로버
+    SPADE("♠"), //♠
+    HEART("♥"), //♥
+    DIAMOND("♦"), //♦
+    CLUB("♣"); //♣
 
     private String icon;
 
